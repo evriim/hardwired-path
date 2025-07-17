36c22fecf2e5f422ee3b80f651da974a289f97b9
Evrim Ustabaş <71992453+evriim@users.noreply.github.com>
2025-07-18 00:29:44 +0300

    Create problem01.c

---
+++ programming-languages/C/project_euler/problem01.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i,sum;
+    for(i = 3; i < 1000; i++){
+        if(i % 3 == 0 || i % 5 == 0){
+            sum+=i;
+        }
+    }
+    printf("%d", sum);
+
+    return 0;
+}
