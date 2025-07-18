972702bfac07b9515465f8822c0c4ee4e98235cc
Evrim Ustabaş <71992453+evriim@users.noreply.github.com>
2025-07-18 12:29:36 +0300

    Create problem03.c

---
+++ programming-languages/C/project_euler/problem03.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+double primeNum(double x){
+    double i;
+    for(i = 2; i < x; i++){
+        if(fmod(x, i) == 0){
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int main()
+{
+    int j;
+    double num = 300425737572;
+    int max = 0;
+    for (j = 2; j < num; j++){
+        if(primeNum(j)){
+                if(fmod(600851475143, j) == 0){
+                    printf("%d \n", j);
+                }
+        }
+    }
+    return 0;
+}
